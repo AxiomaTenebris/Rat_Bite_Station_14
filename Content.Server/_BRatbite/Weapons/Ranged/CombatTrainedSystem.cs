0df9f6112ca0bf428853ed42c7672da6ba7eb13f
Perstronzio Desantis <44839463+PropenzioLavandino@users.noreply.github.com>
2026-05-30 18:00:52 +0200

    Add scared of guns component and remove drop gun mechanics from untrained people

--- Content.Server/_BRatbite/Weapons/Ranged/CombatTrainedSystem.cs
+++ Content.Server/_BRatbite/Weapons/Ranged/CombatTrainedSystem.cs
@@ -1,10 +1,5 @@
-using Content.Shared.Popups;
 using Content.Goobstation.Common.Weapons.Ranged;
 using Content.Shared.Weapons.Ranged.Events;
-using Robust.Shared.Random;
-using Content.Shared.Hands.Components;
-using Content.Shared.Popups;
-using Content.Shared.Hands.EntitySystems;
 using Content.Shared._BRatbite.Weapons.Ranged;
 using Content.Shared.NukeOps;
 
@@ -12,15 +7,13 @@ namespace Content.Server._BRatbite.Weapons.Ranged;
 
 public sealed partial class CombatTrainedSystem : EntitySystem
 {
-    [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly IEntityManager _entityManager = default!;
-    [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
-    [Dependency] private readonly SharedHandsSystem _handsSystem = default!;
+
 
     public override void Initialize()
     {
+        base.Initialize();
         SubscribeLocalEvent<CombatUntrainedComponent, GetRecoilModifiersEvent>(OnRecoilModifiersEvent);
-        SubscribeLocalEvent<CombatUntrainedComponent, ShotAttemptedEvent>(OnShotAttempted);
         SubscribeLocalEvent<CombatTrainedComponent, ComponentStartup>(OnInitializeComp);
         SubscribeLocalEvent<CombatTrainedComponent, ComponentShutdown>(OnShutdownComp);
         SubscribeLocalEvent<NukeOperativeComponent, ComponentStartup>(OnInitializeNukeOps);
@@ -49,23 +42,4 @@ public sealed partial class CombatTrainedSystem : EntitySystem
         args.Modifier = (args.Modifier * ent.Comp.RecoilDebuff) + ent.Comp.FlatRecoilDebuff;
     }
 
-    private void OnShotAttempted(Entity<CombatUntrainedComponent> ent, ref ShotAttemptedEvent args)
-    {
-        if (_random.Prob(ent.Comp.DropGunChance) && TryComp<HandsComponent>(ent.Owner, out var handComp))
-        {
-            if (_handsSystem.TryDrop(new(ent.Owner, handComp)))
-            {
-                _popupSystem.PopupEntity(Loc.GetString("gun-fail-drop-shoot"), ent.Owner, ent.Owner);
-                args.Cancel();
-                return;
-            }
-
-        }
-        if (_random.Prob(ent.Comp.MissShotChance))
-        {
-            _popupSystem.PopupEntity(Loc.GetString("gun-fail-shoot"), ent.Owner, ent.Owner);
-            args.Cancel();
-            return;
-        }
-    }
 }
